e72596ea8182fe0fb7061e3e873d05cd742275b8
Beth Pritchard <bpritchard@scottlogic.com>
2023-09-28 14:53:39 +0100

    Add head

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,6 +7,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className="min-h-screen bg-zinc-900">
+      <head>
+        <title>Crossword Scoreboard</title>
+      </head>
       <body className="w-full h-full text-white">{children}</body>
     </html>
   );
