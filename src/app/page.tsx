f1279edcf17c8bf1be6fed48505afd6626c3b93a
Beth Pritchard <bpritchard@scottlogic.com>
2023-09-29 17:04:27 +0100

    Save to  indexeddb

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,42 @@
 "use client";
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { ScoreButton } from "./components/scoreButton";
 import { INITIAL_SCORE_STATE, Score } from "./types";
 import { B_KEY, CHLO_KEY } from "./constants";
+import { getScore, storeScore } from "./utils";
 
 export default function Home() {
   const [score, setScore] = useState<Score>(INITIAL_SCORE_STATE);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    getScore()
+      .then((retrievedScore?: Score) => {
+        setScore(retrievedScore ?? INITIAL_SCORE_STATE);
+      })
+      .finally(() => {
+        setIsLoading(false);
+      });
+  }, []);
 
   const handleValueChange = (key: keyof Score) => {
-    setScore((prevState: Score) => ({
-      ...prevState,
-      [key]: prevState[key] + 1,
-    }));
-    console.log(score);
+    setScore((prevState: Score) => {
+      const updatedScore = { ...prevState, [key]: prevState[key] + 1 };
+
+      storeScore(updatedScore);
+
+      return updatedScore;
+    });
   };
+
+  if (isLoading) {
+    return (
+      <div className="min-h-screen flex flex-col items-center justify-center align-content font-press text-cyan-300">
+        Loading...
+      </div>
+    );
+  }
 
   return (
     <div className="min-h-screen align-content flex flex-col items-center justify-center font-press">
@@ -22,7 +44,7 @@ export default function Home() {
         <h1 className="underline decoration-1 hover:decoration-cyan-300 decoration-bold decoration-pink-500 text-2xl font-bold mb-6 text-center">
           Crossword Scoreboard
         </h1>
-        <div className="flex justify-center space-x-4">
+        <div className="flex justify-center space-x-4 mb-10">
           <div className="flex flex-col items-center">
             <div className="mb-10">{score[CHLO_KEY]}</div>
             <ScoreButton
@@ -37,6 +59,14 @@ export default function Home() {
               onChange={() => handleValueChange(B_KEY)}
             />
           </div>
+        </div>
+        <div className="flex space-x-4 justify-center">
+          <button className="w-20 md:w-40  bg-pink-400 hover:bg-pink-500 text-white  h-12 text-lg rounded">
+            Submit
+          </button>
+          <button className="w-20 md:w-40  bg-pink-400 hover:bg-pink-500 text-white  h-12 text-lg rounded">
+            Load
+          </button>
         </div>
       </div>
     </div>
